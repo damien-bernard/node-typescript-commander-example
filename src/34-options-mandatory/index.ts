305a1f1089942af86ff0b7ad18be2053ffea8cbd
damien-bernard <155372850+damien-bernard@users.noreply.github.com>
2024-02-07 21:35:56 +0100

    Create Option mandatory

---
+++ src/34-options-mandatory/index.ts
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+import * as commander from 'commander'
+import { version } from '../version.js'
+
+interface Options {
+  port: number;
+}
+
+function print(options: Options):void {
+  console.log(`port is '%s'`, options.port);
+}
+
+const program = new commander.Command();
+program
+  .version('1.0.0')
+  .addOption(new commander.Option('-p, --port <port>', 'port number').makeOptionMandatory(true))
+  .action(print);
+
+program.parse(process.argv);
